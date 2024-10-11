0f36164a2ad6bcb41670afa098d3579c42c681d1
Wallace Vieira da Silva <49287183+wallyboy22@users.noreply.github.com>
2024-10-11 17:28:22 -0300

    Update B_1_0_install_libs.py

--- B_1_0_install_libs.py
+++ B_1_0_install_libs.py
@@ -1,11 +1,10 @@
-# 1. Instalação das bibliotecas necessárias
+import os
 
-# Nesta subetapa, instalamos pacotes essenciais para processamento de imagens e integração com Google Earth Engine e Google Cloud Storage.
+# Instalar bibliotecas com pip
+os.system('pip install rasterio')
+os.system('pip install gcsfs ipywidgets')
 
-!pip install rasterio  # Library for working with geospatial raster data
-!pip install gcsfs ipywidgets  # Instalação de bibliotecas para acesso ao GCS e widgets interativos
-
-# Preparando o ambiente para o GDAL (Geospatial Data Abstraction Library)
-!apt-get install -y libgdal-dev
-!apt-get install -y gdal-bin
-!apt-get install python3-gdal
+# Instalar pacotes com apt-get
+os.system('apt-get install -y libgdal-dev')
+os.system('apt-get install -y gdal-bin')
+os.system('apt-get install -y python3-gdal')
